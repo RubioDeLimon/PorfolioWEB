e43083f1d1245e14da1e6c91deebb40077abc651
rubiodelimon <jorgeantifa@gmail.com>
2022-06-14 20:16:30 +0100

    Swiper

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,53 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    tittle: 'Crypto Currency Dashboard & Financial Visualization',
+    github: "https://github.com/RubioDeLimon",
+    demo: "https://dribbble.com/Alien_pixels",
+  },
+
+]
+
 export const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,66 +61,24 @@ export const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/RubioDeLimon" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+        
+        
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,75 @@
 import React from 'react';
 import './testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
-const Testimonials = () =>{
-    return(
-        <section id='testimonials'>Testimonials</section>
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: 'Tina Snow',
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, modi repellendus numquam ullam eum itaque magni pariatur accusantium deserunt quibusdam deleniti aspernatur culpa unde eaque nobis libero impedit debitis maxime.",
+
+    },
+    {
+        avatar: AVTR2,
+        name: 'Tina Snow',
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, modi repellendus numquam ullam eum itaque magni pariatur accusantium deserunt quibusdam deleniti aspernatur culpa unde eaque nobis libero impedit debitis maxime.",
+
+    },
+    {
+        avatar: AVTR3,
+        name: 'Tono Snow',
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, modi repellendus numquam ullam eum itaque magni pariatur accusantium deserunt quibusdam deleniti aspernatur culpa unde eaque nobis libero impedit debitis maxime.",
+
+    },
+    {
+        avatar: AVTR4,
+        name: 'Tono Snow',
+        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, modi repellendus numquam ullam eum itaque magni pariatur accusantium deserunt quibusdam deleniti aspernatur culpa unde eaque nobis libero impedit debitis maxime.",
+
+    },
+]
+
+const Testimonials = () => {
+    return (
+        <section id='testimonials'>
+            <h5>Review from clients</h5>
+            <h2>Testimonials</h2>
+
+            <Swiper className="container testimonials__container"
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true}}>
+                {
+                    data.map(({avatar, name, review}, index) => {
+                        return(
+                            <SwiperSlide className='testimonial'>
+                                <div className="client__avatar">
+                                    <img src={avatar} alt="Avatar One" />
+                                </div>
+                                <h5>{name}</h5>
+                                <small className='client_review'> {review} </small>
+
+                            </SwiperSlide>
+                    )
+                    })
+                }
+            </Swiper>
+            
+        </section>
+
     )
 }
 
